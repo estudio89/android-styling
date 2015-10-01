c0f37bdd939349f431d3a90d7cff65e3af41319a
Luccas Correa <luccascorrea@estudio89.com.br>
2015-10-01 08:36:14 -0300

    First commit

---
+++ src/main/java/br/com/estudio89/styling/App.java
@@ -0,0 +1,13 @@
+package br.com.estudio89;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
